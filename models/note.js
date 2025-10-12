30a6df9bb4994d990ef85646153a1f8ce517c7ea
Joel Kim <joelminchankim@gmail.com>
2025-10-12 18:21:02 -0400

    refactor: clean up project structure

--- models/note.js
+++ models/note.js
@@ -1,26 +1,10 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-console.log('Using MongoDB URL:', url)
-
-console.log('connecting to', url)
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -30,7 +14,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  },
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
